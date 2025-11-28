e10820b1a2cf7c8b6841e326535cd6b12bad0bd6
Sk Md Rohan <127346438+rohan2by1@users.noreply.github.com>
2025-11-28 11:59:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-import express from "express";
-import { createProxyMiddleware } from "http-proxy-middleware";
+const express = require("express");
+const httpProxy = require("http-proxy");
 
 const app = express();
 
-app.use(
-  "/", 
-  createProxyMiddleware({
-    target: "https://neko.rohan2by1.dev",
-    changeOrigin: true,
-    ws: true,         // if Neko uses WebSockets
-    secure: false
-  })
-);
+const TARGET = process.env.TARGET_URL; 
+// Example: "http://4.188.80.161:8080"
+
+const proxy = httpProxy.createProxyServer({
+  target: TARGET,
+  ws: true,
+  changeOrigin: true,
+  secure: false
+});
+
+// HTTP Proxy
+app.use((req, res) => {
+  proxy.web(req, res);
+});
+
+// WebSocket Proxy
+app.on("upgrade", (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
 
 const port = process.env.PORT || 10000;
-app.listen(port, () => console.log("Proxy running on port " + port));
+app.listen(port, () => {
+  console.log("Reverse proxy running on port", port);
+});
